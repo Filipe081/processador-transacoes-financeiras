d33dd2ccf04698e34477450ceac6db4ae9085119
Filipe <filipejoserds123@gmail.com>
2025-06-07 01:42:40 -0300

    Att no app.py

--- app.py
+++ app.py
@@ -51,7 +51,4 @@ def historico():
     return render_template('historico.html', transacoes=transacoes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
